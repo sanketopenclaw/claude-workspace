5a14c88a6b221e6b5146a583ac11fcf92d458be1
sanketopenclaw <sanket.openclaw@gmail.com>
2026-07-05 12:27:17 +0530

    Add UDP telemetry listener with packet dispatch

---
+++ f1-race-engineer/telemetry/listener.py
@@ -0,0 +1,57 @@
+import socket
+import time
+from telemetry import packets
+
+PACKET_ID_LAP_DATA = 2
+PACKET_ID_CAR_STATUS = 7
+PACKET_ID_CAR_DAMAGE = 10
+
+
+class TelemetryListener:
+    def __init__(self, state_tracker, ip="0.0.0.0", port=20777, capture_writer=None):
+        self.state_tracker = state_tracker
+        self.ip = ip
+        self.port = port
+        self.capture_writer = capture_writer
+        self._sock = None
+        self._running = False
+
+    def start(self):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.bind((self.ip, self.port))
+        self._sock.settimeout(1.0)
+        self._running = True
+        last_packet_time = time.time()
+        warned = False
+        while self._running:
+            try:
+                data, _ = self._sock.recvfrom(2048)
+                last_packet_time = time.time()
+                warned = False
+            except socket.timeout:
+                if not warned and time.time() - last_packet_time > 5:
+                    print("[telemetry] no packets received in 5s - check game UDP settings")
+                    warned = True
+                continue
+            if self.capture_writer:
+                self.capture_writer(data)
+            self._dispatch(data)
+
+    def stop(self):
+        self._running = False
+        if self._sock:
+            self._sock.close()
+
+    def _dispatch(self, data):
+        header = packets.parse_header(data)
+        player_car_index = header["player_car_index"]
+        packet_id = header["packet_id"]
+        if packet_id == PACKET_ID_LAP_DATA:
+            my_lap, gap_ahead_ms, gap_behind_ms = packets.parse_lap_data_packet(data, player_car_index)
+            self.state_tracker.update_lap_data(my_lap, gap_ahead_ms, gap_behind_ms)
+        elif packet_id == PACKET_ID_CAR_STATUS:
+            fuel_in_tank, fuel_remaining_laps = packets.parse_car_status_packet(data, player_car_index)
+            self.state_tracker.update_car_status(fuel_in_tank, fuel_remaining_laps)
+        elif packet_id == PACKET_ID_CAR_DAMAGE:
+            tyres_wear = packets.parse_car_damage_packet(data, player_car_index)
+            self.state_tracker.update_car_damage(tyres_wear)
